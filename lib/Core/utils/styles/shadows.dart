7c0e46835b475fcaab9e64ba10a35d327ee1b3b5
George-Abboud <geo1111ang@gmail.com>
2024-12-12 18:56:43 +0300

    add glass drop shadow

--- lib/Core/utils/styles/shadows.dart
+++ lib/Core/utils/styles/shadows.dart
@@ -1,4 +1,3 @@
-import 'package:e_delivery_app/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Shadows {
@@ -6,6 +5,11 @@ abstract class Shadows {
     offset: Offset(4, 4),
     blurRadius: 15,
     color: Color.fromARGB(26, 0, 0, 0),
+  );
+  static const BoxShadow glassDropShadow = BoxShadow(
+    offset: Offset(0, 8),
+    blurRadius: 10,
+    color: Color.fromARGB(64, 0, 0, 0),
   );
   static const BoxShadow ctaDropShadow = BoxShadow(
     offset: Offset(6, 8),
