b180ceb8ee3ee8525025f347e24f6478074fbd62
George-Abboud <geo1111ang@gmail.com>
2024-12-20 19:30:06 +0300

    create custom cart app bar

--- lib/Features/Cart/Presentation/Views/cart_view.dart
+++ lib/Features/Cart/Presentation/Views/cart_view.dart
@@ -1,4 +1,5 @@
 import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/cart_products_list_view.dart';
+import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/custom_cart_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class CartView extends StatelessWidget {
@@ -7,6 +8,7 @@ class CartView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      appBar: CustomCartAppBar(),
       body: CartProductsListView(),
     );
   }
