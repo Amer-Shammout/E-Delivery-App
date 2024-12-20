3dfe3a525c1e5e5763ccecc99aebec40785f293a
George-Abboud <geo1111ang@gmail.com>
2024-12-20 20:12:15 +0300

    create custom bottom sheet

--- lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/custom_cart_check_box.dart
+++ lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/custom_cart_check_box.dart
@@ -1,4 +1,5 @@
 import 'package:e_delivery_app/Core/widgets/custom_check_box.dart';
+import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/cart_bottom_sheet.dart';
 import 'package:flutter/material.dart';
 
 class CustomCartCheckBox extends StatefulWidget {
@@ -15,9 +16,29 @@ class _CustomCartCheckBoxState extends State<CustomCartCheckBox> {
     return GestureDetector(
       onTap: () {
         value = !value;
+        if (value) {
+          _customBottomSheet(context);
+        }
+
         setState(() {});
       },
       child: CustomCheckBox(value: value),
+    );
+  }
+
+  Future<dynamic> _customBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      context: context,
+      builder: (context) {
+        return const CartBottomSheet();
+      },
     );
   }
 }
