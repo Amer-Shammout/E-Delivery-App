112894508993b42036ea9212d647812dc61da3a7
AmerShammout <amershammout2004@gmail.com>
2024-12-27 19:18:52 +0300

    create registeration_model.dart

---
+++ lib/Features/Auth/Data/Models/registeration_model.dart
@@ -0,0 +1,12 @@
+class RegisterationModel {
+  final String phoneNumber, fcmToken;
+
+  RegisterationModel({required this.phoneNumber, required this.fcmToken});
+
+   Map<String, dynamic> toJson() {
+    return {
+      'fcm_token': fcmToken,
+      'phone_number': phoneNumber,
+    };
+  }
+}
