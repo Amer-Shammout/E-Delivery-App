93ade0e860771cd2c0f64a0a5bc1c6329d79abb5
George-Abboud <geo1111ang@gmail.com>
2024-12-28 19:23:10 +0300

    trigger verification cubit

--- lib/Features/Auth/Presentation/Views/setting_info_view.dart
+++ lib/Features/Auth/Presentation/Views/setting_info_view.dart
@@ -1,9 +1,12 @@
+import 'package:e_delivery_app/Features/Auth/Data/Models/verification_response_model/verification_response_model.dart';
 import 'package:e_delivery_app/Features/Auth/Presentation/Views/widgets/Setting%20Info/setting_info_body.dart';
 import 'package:e_delivery_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class SettingInfoView extends StatelessWidget {
-  const SettingInfoView({super.key});
+  const SettingInfoView({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
