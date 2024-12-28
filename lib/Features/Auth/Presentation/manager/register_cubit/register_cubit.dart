b162c3b6018c77f44a0505a58cf9ede832e94913
AmerShammout <amershammout2004@gmail.com>
2024-12-28 14:11:12 +0300

    register cubit done

---
+++ lib/Features/Auth/Presentation/manager/register_cubit/register_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_delivery_app/Features/Auth/Data/Models/registeration_model.dart';
+import 'package:e_delivery_app/Features/Auth/Data/repos/auth_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit(this._authRepo) : super(RegisterInitial());
+
+  final AuthRepo _authRepo;
+
+  Future<void> register(RegisterationModel registerModel) async {
+    emit(RegisterLoading());
+    var result = await _authRepo.register(registerModel);
+
+    result.fold((failure) {
+      emit(RegisterFailure(errMessage: failure.errMessage));
+    }, (flag) {
+      emit(RegisterSuccess());
+    });
+  }
+}
