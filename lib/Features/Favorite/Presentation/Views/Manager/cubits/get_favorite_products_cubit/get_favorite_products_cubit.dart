9bce5cfcca13e84a4e0f8f5a092f57fd5a2c476e
George-Abboud <geo1111ang@gmail.com>
2025-01-09 21:59:58 +0300

    ceate get favorite products cubit

---
+++ lib/Features/Favorite/Presentation/Views/Manager/cubits/get_favorite_products_cubit/get_favorite_products_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:e_delivery_app/Core/Data/Models/product_model/product_model.dart';
+import 'package:e_delivery_app/Features/Favorite/Data/Repos/get_favorite_products_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'get_favorite_products_state.dart';
+
+class GetFavoriteProductsCubit extends Cubit<GetFavoriteProductsState> {
+  GetFavoriteProductsCubit(this._getFavoriteProductsRepo)
+      : super(GetFavoriteProductsInitial());
+  final GetFavoriteProductsRepo _getFavoriteProductsRepo;
+  Future<void> getFavoriteProducts() async {
+    emit(GetFavoriteProductsLoading());
+    var result = await _getFavoriteProductsRepo.getFavoriteProducts();
+    result.fold(
+      (failure) {
+        emit(GetFavoriteProductsFailure(errMessage: failure.errMessage));
+      },
+      (favoriteProducts) {
+        emit(GetFavoriteProductsSuccess(favoriteProducts: favoriteProducts));
+      },
+    );
+  }
+}
---
+++ lib/Features/Favorite/Presentation/Views/Manager/cubits/get_favorite_products_cubit/get_favorite_products_state.dart
@@ -0,0 +1,24 @@
+part of 'get_favorite_products_cubit.dart';
+
+sealed class GetFavoriteProductsState extends Equatable {
+  const GetFavoriteProductsState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class GetFavoriteProductsInitial extends GetFavoriteProductsState {}
+
+final class GetFavoriteProductsLoading extends GetFavoriteProductsState {}
+
+final class GetFavoriteProductsSuccess extends GetFavoriteProductsState {
+  final List<ProductModel> favoriteProducts;
+
+  const GetFavoriteProductsSuccess({required this.favoriteProducts});
+}
+
+final class GetFavoriteProductsFailure extends GetFavoriteProductsState {
+  final String errMessage;
+
+  const GetFavoriteProductsFailure({required this.errMessage});
+}
