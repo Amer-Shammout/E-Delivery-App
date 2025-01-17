64df5a987469a5646280f40644872666f37ab3b3
George-Abboud <geo1111ang@gmail.com>
2025-01-17 11:07:05 +0300

    delete from cart inside cart done

--- lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/custom_delete_button.dart
+++ lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/custom_delete_button.dart
@@ -1,13 +1,20 @@
+import 'package:e_delivery_app/Core/Data/Manager/remove_from_cart_cubit/remove_from_cart_cubit.dart';
 import 'package:e_delivery_app/Core/utils/assets.dart';
 import 'package:e_delivery_app/Core/utils/styles/app_styles.dart';
+import 'package:e_delivery_app/Features/Cart/Presentation/manager/edit_quantity_cubit/edit_quantity_cubit.dart';
+import 'package:e_delivery_app/Features/Cart/Presentation/manager/get_cart_cubit/get_cart_cubit.dart';
+import 'package:e_delivery_app/Features/Cart/data/models/cart_model/cart_item_quantity/order_item.dart';
 import 'package:e_delivery_app/constants.dart';
 import 'package:e_delivery_app/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomDeleteButton extends StatelessWidget {
-  const CustomDeleteButton({super.key});
-
+  const CustomDeleteButton(
+      {super.key, required this.productId, required this.index});
+  final int index;
+  final int productId;
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -22,7 +29,14 @@ class CustomDeleteButton extends StatelessWidget {
               borderRadius: BorderRadius.circular(16),
             ),
           ),
-          onPressed: () {},
+          onPressed: () async {
+            await BlocProvider.of<RemoveFromCartCubit>(context)
+                .removeFromCart(productId);
+            BlocProvider.of<EditQuantityCubit>(context)
+                .cartItemQuantity!
+                .orderItems!
+                .removeAt(index);
+          },
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
--- lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/product_slidable.dart
+++ lib/Features/Cart/Presentation/Views/Widgets/Cart View Widgets/product_slidable.dart
@@ -1,9 +1,13 @@
+import 'package:e_delivery_app/Core/Data/Manager/remove_from_cart_cubit/remove_from_cart_cubit.dart';
+import 'package:e_delivery_app/Core/Data/Repos/app_repo_impl.dart';
+import 'package:e_delivery_app/Core/services/service_locator.dart';
 import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/custom_cart_check_box.dart';
 import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/custom_delete_button.dart';
 import 'package:e_delivery_app/Features/Cart/Presentation/Views/Widgets/Cart%20View%20Widgets/product_card_cart.dart';
 import 'package:e_delivery_app/Features/Cart/data/models/cart_model/order_item.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ProductSlidable extends StatelessWidget {
@@ -14,28 +18,36 @@ class ProductSlidable extends StatelessWidget {
   final int index;
   @override
   Widget build(BuildContext context) {
-    return SlidableAutoCloseBehavior(
-      closeWhenTapped: false,
-      closeWhenOpened: true,
-      child: Slidable(
-        dragStartBehavior: DragStartBehavior.start,
-        endActionPane: ActionPane(
-          extentRatio: 88 / MediaQuery.sizeOf(context).width,
-          motion: const ScrollMotion(),
-          children: const [CustomDeleteButton()],
-        ),
-        child: Row(
-          children: [
-            const SizedBox(
-              width: 16,
-            ),
-            Expanded(
-              child: ProductCardCart(orderItem: orderItem, index: index),
-            ),
-            const SizedBox(
-              width: 16,
-            ),
-          ],
+    return BlocProvider(
+      create: (context) => RemoveFromCartCubit(getIt.get<AppRepoImpl>()),
+      child: SlidableAutoCloseBehavior(
+        closeWhenTapped: false,
+        closeWhenOpened: true,
+        child: Slidable(
+          dragStartBehavior: DragStartBehavior.start,
+          endActionPane: ActionPane(
+            extentRatio: 88 / MediaQuery.sizeOf(context).width,
+            motion: const ScrollMotion(),
+            children: [
+              CustomDeleteButton(
+                productId: orderItem.productDetails!.id!,
+                index: index,
+              )
+            ],
+          ),
+          child: Row(
+            children: [
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: ProductCardCart(orderItem: orderItem, index: index),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+            ],
+          ),
         ),
       ),
     );
